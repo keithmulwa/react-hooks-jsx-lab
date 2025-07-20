820f3615e255fe6b0abac903222bb4223de92219
keithmulwa <150774544+keithmulwa@users.noreply.github.com>
2025-07-20 10:26:04 +0300

    First

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+function About() {//<div>About</div>;
+   return(
+<div id ="about">
+  <h2>About Me</h2>
+  <p>My name is Mulwa</p>
+  <img src="https://i.imgur.com/mV8PQxj.gif"
+  alt="I made this"
+  />
+</div>
+   );                           
 }
 
 export default About;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,11 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+<NavBar>
+  <a href="#home">Home</a>
+  <a href="#about">About</a>
+</NavBar>);
 }
 
 export default NavBar;
